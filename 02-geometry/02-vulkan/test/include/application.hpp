d6323b5753b51b8ff78ffd510a81c05f2b346ad6
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-10-21 17:24:16 +0300

    set up application and engine files

---
+++ 02-geometry/02-vulkan/test/include/application.hpp
@@ -0,0 +1,18 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <tsimmerman.ss@phystech.edu>, <alex.rom23@mail.ru> wrote this file.  As long as you
+ * retain this notice you can do whatever you want with this stuff. If we meet
+ * some day, and you think this stuff is worth it, you can buy me a beer in
+ * return.
+ * ----------------------------------------------------------------------------
+ */
+
+#pragma once
+
+namespace triangles {
+class application final {
+public:
+  void run() {}
+};
+} // namespace triangles
