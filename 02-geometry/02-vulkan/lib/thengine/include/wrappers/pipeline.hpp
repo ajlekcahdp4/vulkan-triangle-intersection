0658dc512f6dfee88b41ecdb333896f301d7e0cf
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-11-20 16:06:32 +0300

    refactor pipeline

--- 02-geometry/02-vulkan/lib/thengine/include/wrappers/pipeline.hpp
+++ 02-geometry/02-vulkan/lib/thengine/include/wrappers/pipeline.hpp
@@ -41,33 +41,10 @@ public:
 private:
   static vk::raii::DescriptorSetLayout create_decriptor_set_layout(
       const vk::raii::Device                                                            &p_device,
-      const std::vector<std::tuple<vk::DescriptorType, uint32_t, vk::ShaderStageFlags>> &p_binding_data) {
-    std::vector<vk::DescriptorSetLayoutBinding> bindings{static_cast<uint32_t>(p_binding_data.size())};
-
-    for (uint32_t i = 0; i < p_binding_data.size(); ++i) {
-      bindings.push_back(vk::DescriptorSetLayoutBinding{
-          i, std::get<0>(p_binding_data[i]), std::get<1>(p_binding_data[i]), std::get<2>(p_binding_data[i])});
-    }
-
-    vk::DescriptorSetLayoutCreateInfo descriptor_set_info = {.bindingCount = static_cast<uint32_t>(bindings.size()),
-                                                             .pBindings = bindings.data()};
-
-    return vk::raii::DescriptorSetLayout{p_device, descriptor_set_info};
-  }
+      const std::vector<std::tuple<vk::DescriptorType, uint32_t, vk::ShaderStageFlags>> &p_binding_data);
 
   static vk::raii::DescriptorPool create_descriptor_pool(const vk::raii::Device                    &p_device,
-                                                         const std::vector<vk::DescriptorPoolSize> &p_pool_sizes) {
-    uint32_t max_sets =
-        std::accumulate(p_pool_sizes.begin(), p_pool_sizes.end(), 0,
-                        [](uint32_t sum, vk::DescriptorPoolSize const &dps) { return sum + dps.descriptorCount; });
-
-    vk::DescriptorPoolCreateInfo descriptor_info = {.flags = vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet,
-                                                    .maxSets = max_sets,
-                                                    .poolSizeCount = static_cast<uint32_t>(p_pool_sizes.size()),
-                                                    .pPoolSizes = p_pool_sizes.data()};
-
-    return vk::raii::DescriptorPool(p_device, descriptor_info);
-  }
+                                                         const std::vector<vk::DescriptorPoolSize> &p_pool_sizes);
 };
 
 template <class vertex_t> struct pipeline_data {
@@ -87,127 +64,156 @@ public:
 
 private:
   vk::raii::Pipeline create_pipeline(const vk::raii::Device &p_device, const std::string &p_vertex_file_path,
-                                     const std::string &p_fragment_file_path, const vk::Extent2D &p_extent) {
-    // vertex input
-    vk::PipelineVertexInputStateCreateInfo vertex_input_info{};
-    vertex_input_info.flags = vk::PipelineVertexInputStateCreateFlags();
-    vertex_input_info.vertexBindingDescriptionCount = 0;
-    vertex_input_info.vertexAttributeDescriptionCount = 0;
-    auto binding_description = vertex_t::get_binding_description();
-    auto attribute_description = vertex_t::get_attribute_description();
-    vertex_input_info.vertexBindingDescriptionCount = 1;
-    vertex_input_info.vertexAttributeDescriptionCount = attribute_description.size();
-    vertex_input_info.pVertexBindingDescriptions = &binding_description;
-    vertex_input_info.pVertexAttributeDescriptions = attribute_description.data();
-
-    // input assembly
-    vk::PipelineInputAssemblyStateCreateInfo input_asm_info{};
-    input_asm_info.flags = vk::PipelineInputAssemblyStateCreateFlags();
-    input_asm_info.topology = vk::PrimitiveTopology::eTriangleList;
-
-    // Viewport and scissor
-    vk::Viewport viewport = {0.0f, 0.0f, static_cast<float>(p_extent.width), static_cast<float>(p_extent.height),
-                             0.0f, 1.0f};
-    vk::Rect2D   scissor = {vk::Offset2D{0, 0}, p_extent};
-
-    vk::PipelineViewportStateCreateInfo viewport_info = {
-        .viewportCount = 1, .pViewports = &viewport, .scissorCount = 1, .pScissors = &scissor};
-
-    // Rasterisation
-    vk::PipelineRasterizationStateCreateInfo rasterization_info = {
-        .depthClampEnable = VK_FALSE,
-        .rasterizerDiscardEnable = VK_FALSE,
-        .polygonMode = vk::PolygonMode::eFill,
-        .cullMode = vk::CullModeFlagBits::eBack,
-        .frontFace = vk::FrontFace::eClockwise,
-        .depthBiasEnable = VK_FALSE,
-        .lineWidth = 1.0f,
-    };
-
-    // Multisampling
-    vk::PipelineMultisampleStateCreateInfo multisampling = {.rasterizationSamples = vk::SampleCountFlagBits::e1,
-                                                            .sampleShadingEnable = VK_FALSE};
-
-    // color blend
-    vk::PipelineColorBlendAttachmentState color_blend_attachments = {
-        .blendEnable = VK_FALSE,
-        .colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG |
-                          vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA,
-    };
-
-    vk::PipelineColorBlendStateCreateInfo color_blending = {
-        .logicOpEnable = VK_FALSE,
-        .logicOp = vk::LogicOp::eCopy,
-        .attachmentCount = 1,
-        .pAttachments = &color_blend_attachments,
-        .blendConstants = {},
-    };
-
-    // shader stages
-    std::vector<vk::PipelineShaderStageCreateInfo> shader_stages;
-
-    // vertex shader
-    auto                              vertex_shader = create_module(p_vertex_file_path, p_device);
-    vk::PipelineShaderStageCreateInfo vertex_shader_info = {
-        .stage = vk::ShaderStageFlagBits::eVertex, .module = *vertex_shader, .pName = "main"};
-    shader_stages.push_back(vertex_shader_info);
-
-    // fragment shader
-    auto                              fragment_shader = create_module(p_fragment_file_path, p_device);
-    vk::PipelineShaderStageCreateInfo fragment_shader_info = {
-        .stage = vk::ShaderStageFlagBits::eFragment, .module = *fragment_shader, .pName = "main"};
-    shader_stages.push_back(fragment_shader_info);
-
-    vk::GraphicsPipelineCreateInfo pipeline_info = {.stageCount = static_cast<uint32_t>(shader_stages.size()),
-                                                    .pStages = shader_stages.data(),
-                                                    .pVertexInputState = &vertex_input_info,
-                                                    .pInputAssemblyState = &input_asm_info,
-                                                    .pViewportState = &viewport_info,
-                                                    .pRasterizationState = &rasterization_info,
-                                                    .pMultisampleState = &multisampling,
-                                                    .pColorBlendState = &color_blending,
-                                                    .layout = *m_layout,
-                                                    .renderPass = *m_render_pass,
-                                                    .subpass = 0,
-                                                    .basePipelineHandle = nullptr};
-
-    return p_device.createGraphicsPipeline(nullptr, pipeline_info);
-  }
+                                     const std::string &p_fragment_file_path, const vk::Extent2D &p_extent);
 
   static vk::raii::PipelineLayout create_pipeline_layout(const vk::raii::Device              &device,
-                                                         const vk::raii::DescriptorSetLayout &p_descriptor_set_layout) {
-    vk::PipelineLayoutCreateInfo layout_info{};
-    layout_info.flags = vk::PipelineLayoutCreateFlags();
-    layout_info.setLayoutCount = 1;
-    vk::DescriptorSetLayout set_layouts[] = {*p_descriptor_set_layout};
-    layout_info.pSetLayouts = set_layouts;
-    layout_info.pushConstantRangeCount = 0;
-    return device.createPipelineLayout(layout_info);
-  }
+                                                         const vk::raii::DescriptorSetLayout &p_descriptor_set_layout);
 
-  static vk::raii::RenderPass create_render_pass(const vk::raii::Device &p_device) {
-    vk::AttachmentDescription color_attachment = {.flags = vk::AttachmentDescriptionFlags(),
-                                                  .format = vk::Format::eB8G8R8A8Unorm,
-                                                  .samples = vk::SampleCountFlagBits::e1,
-                                                  .loadOp = vk::AttachmentLoadOp::eClear,
-                                                  .storeOp = vk::AttachmentStoreOp::eStore,
-                                                  .stencilLoadOp = vk::AttachmentLoadOp::eDontCare,
-                                                  .stencilStoreOp = vk::AttachmentStoreOp::eDontCare,
-                                                  .initialLayout = vk::ImageLayout::eUndefined,
-                                                  .finalLayout = vk::ImageLayout::ePresentSrcKHR};
+  static vk::raii::RenderPass create_render_pass(const vk::raii::Device &p_device);
 
-    vk::AttachmentReference color_attachment_ref = {.attachment = 0,
-                                                    .layout = vk::ImageLayout::eColorAttachmentOptimal};
+  static vk::PipelineVertexInputStateCreateInfo
+  vertex_input_state_create_info(vk::VertexInputBindingDescription                  &binding_description,
+                                 std::array<vk::VertexInputAttributeDescription, 2> &attribute_description);
 
-    vk::SubpassDescription subpass = {.pipelineBindPoint = vk::PipelineBindPoint::eGraphics,
-                                      .colorAttachmentCount = 1,
-                                      .pColorAttachments = &color_attachment_ref};
+  static vk::PipelineRasterizationStateCreateInfo rasterization_state_create_info();
 
-    vk::RenderPassCreateInfo renderpass_info = {
-        .attachmentCount = 1, .pAttachments = &color_attachment, .subpassCount = 1, .pSubpasses = &subpass};
+  static vk::PipelineColorBlendStateCreateInfo
+  color_blend_state_create_info(vk::PipelineColorBlendAttachmentState &color_attachments);
 
-    return p_device.createRenderPass(renderpass_info);
-  }
-}; // namespace graphics
+  static vk::PipelineColorBlendAttachmentState color_blend_attachments();
+};
+
+template <class vertex_t>
+vk::raii::Pipeline
+pipeline_data<vertex_t>::create_pipeline(const vk::raii::Device &p_device, const std::string &p_vertex_file_path,
+                                         const std::string &p_fragment_file_path, const vk::Extent2D &p_extent) {
+  auto binding_description = vertex_t::get_binding_description();
+  auto attribute_description = vertex_t::get_attribute_description();
+  auto vertex_input_info = vertex_input_state_create_info(binding_description, attribute_description);
+  auto rasterization_info = rasterization_state_create_info();
+  auto color_attachments = color_blend_attachments();
+  auto color_blend_info = color_blend_state_create_info(color_attachments);
+  vk::PipelineInputAssemblyStateCreateInfo input_asm_info{.flags = vk::PipelineInputAssemblyStateCreateFlags(),
+                                                          .topology = vk::PrimitiveTopology::eTriangleList};
+  vk::Viewport viewport = {0.0f, 0.0f, static_cast<float>(p_extent.width), static_cast<float>(p_extent.height),
+                           0.0f, 1.0f};
+  vk::Rect2D   scissor = {vk::Offset2D{0, 0}, p_extent};
+  vk::PipelineViewportStateCreateInfo viewport_info = {
+      .viewportCount = 1, .pViewports = &viewport, .scissorCount = 1, .pScissors = &scissor};
+  vk::PipelineMultisampleStateCreateInfo         multisampling = {.rasterizationSamples = vk::SampleCountFlagBits::e1,
+                                                                  .sampleShadingEnable = VK_FALSE};
+  std::vector<vk::PipelineShaderStageCreateInfo> shader_stages;
+
+  // vertex shader
+  auto                              vertex_shader = create_module(p_vertex_file_path, p_device);
+  vk::PipelineShaderStageCreateInfo vertex_shader_info = {
+      .stage = vk::ShaderStageFlagBits::eVertex, .module = *vertex_shader, .pName = "main"};
+  shader_stages.push_back(vertex_shader_info);
+
+  // fragment shader
+  auto                              fragment_shader = create_module(p_fragment_file_path, p_device);
+  vk::PipelineShaderStageCreateInfo fragment_shader_info = {
+      .stage = vk::ShaderStageFlagBits::eFragment, .module = *fragment_shader, .pName = "main"};
+  shader_stages.push_back(fragment_shader_info);
+
+  vk::GraphicsPipelineCreateInfo pipeline_info = {.stageCount = static_cast<uint32_t>(shader_stages.size()),
+                                                  .pStages = shader_stages.data(),
+                                                  .pVertexInputState = &vertex_input_info,
+                                                  .pInputAssemblyState = &input_asm_info,
+                                                  .pViewportState = &viewport_info,
+                                                  .pRasterizationState = &rasterization_info,
+                                                  .pMultisampleState = &multisampling,
+                                                  .pColorBlendState = &color_blend_info,
+                                                  .layout = *m_layout,
+                                                  .renderPass = *m_render_pass,
+                                                  .subpass = 0,
+                                                  .basePipelineHandle = nullptr};
+
+  return p_device.createGraphicsPipeline(nullptr, pipeline_info);
+}
+
+template <class vertex_t>
+vk::raii::PipelineLayout
+pipeline_data<vertex_t>::create_pipeline_layout(const vk::raii::Device              &device,
+                                                const vk::raii::DescriptorSetLayout &p_descriptor_set_layout) {
+  vk::PipelineLayoutCreateInfo layout_info{};
+  layout_info.flags = vk::PipelineLayoutCreateFlags();
+  layout_info.setLayoutCount = 1;
+  vk::DescriptorSetLayout set_layouts[] = {*p_descriptor_set_layout};
+  layout_info.pSetLayouts = set_layouts;
+  layout_info.pushConstantRangeCount = 0;
+  return device.createPipelineLayout(layout_info);
+}
+
+template <class vertex_t>
+vk::raii::RenderPass pipeline_data<vertex_t>::create_render_pass(const vk::raii::Device &p_device) {
+  vk::AttachmentDescription color_attachment = {.flags = vk::AttachmentDescriptionFlags(),
+                                                .format = vk::Format::eB8G8R8A8Unorm,
+                                                .samples = vk::SampleCountFlagBits::e1,
+                                                .loadOp = vk::AttachmentLoadOp::eClear,
+                                                .storeOp = vk::AttachmentStoreOp::eStore,
+                                                .stencilLoadOp = vk::AttachmentLoadOp::eDontCare,
+                                                .stencilStoreOp = vk::AttachmentStoreOp::eDontCare,
+                                                .initialLayout = vk::ImageLayout::eUndefined,
+                                                .finalLayout = vk::ImageLayout::ePresentSrcKHR};
+
+  vk::AttachmentReference color_attachment_ref = {.attachment = 0, .layout = vk::ImageLayout::eColorAttachmentOptimal};
+
+  vk::SubpassDescription subpass = {.pipelineBindPoint = vk::PipelineBindPoint::eGraphics,
+                                    .colorAttachmentCount = 1,
+                                    .pColorAttachments = &color_attachment_ref};
+
+  vk::RenderPassCreateInfo renderpass_info = {
+      .attachmentCount = 1, .pAttachments = &color_attachment, .subpassCount = 1, .pSubpasses = &subpass};
+
+  return p_device.createRenderPass(renderpass_info);
+}
+
+template <class vertex_t>
+vk::PipelineVertexInputStateCreateInfo pipeline_data<vertex_t>::vertex_input_state_create_info(
+    vk::VertexInputBindingDescription                  &binding_description,
+    std::array<vk::VertexInputAttributeDescription, 2> &attribute_description) {
+  return vk::PipelineVertexInputStateCreateInfo{.flags = vk::PipelineVertexInputStateCreateFlags(),
+                                                .vertexBindingDescriptionCount = 1,
+                                                .pVertexBindingDescriptions = &binding_description,
+                                                .vertexAttributeDescriptionCount = attribute_description.size(),
+                                                .pVertexAttributeDescriptions = attribute_description.data()
+
+  };
+}
+
+template <class vertex_t>
+vk::PipelineRasterizationStateCreateInfo pipeline_data<vertex_t>::rasterization_state_create_info() {
+  return vk::PipelineRasterizationStateCreateInfo{
+      .depthClampEnable = VK_FALSE,
+      .rasterizerDiscardEnable = VK_FALSE,
+      .polygonMode = vk::PolygonMode::eFill,
+      .cullMode = vk::CullModeFlagBits::eBack,
+      .frontFace = vk::FrontFace::eClockwise,
+      .depthBiasEnable = VK_FALSE,
+      .lineWidth = 1.0f,
+  };
+}
+
+template <class vertex_t> vk::PipelineColorBlendAttachmentState pipeline_data<vertex_t>::color_blend_attachments() {
+  return vk::PipelineColorBlendAttachmentState{
+      .blendEnable = VK_FALSE,
+      .colorWriteMask = vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG |
+                        vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA,
+  };
+}
+
+template <class vertex_t>
+vk::PipelineColorBlendStateCreateInfo
+pipeline_data<vertex_t>::color_blend_state_create_info(vk::PipelineColorBlendAttachmentState &color_attachments) {
+  return vk::PipelineColorBlendStateCreateInfo{
+      .logicOpEnable = VK_FALSE,
+      .logicOp = vk::LogicOp::eCopy,
+      .attachmentCount = 1,
+      .pAttachments = &color_attachments,
+      .blendConstants = {},
+  };
+}
+
 } // namespace graphics
 } // namespace throttle
