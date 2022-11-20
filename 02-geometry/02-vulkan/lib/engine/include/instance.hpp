60b913debae6a471d4ce25368fa93a15e13b79fd
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-11-20 11:19:59 +0300

    solve conflicts

---
+++ 02-geometry/02-vulkan/lib/engine/include/instance.hpp
@@ -0,0 +1,109 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <tsimmerman.ss@phystech.edu>, <alex.rom23@mail.ru> wrote this file.  As long as you
+ * retain this notice you can do whatever you want with this stuff. If we meet
+ * some day, and you think this stuff is worth it, you can buy me a beer in
+ * return.
+ * ----------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#include <vulkan/vulkan_raii.hpp>
+#define GLFW_INCLUDE_NONE
+#include "logging.hpp"
+#include <GLFW/glfw3.h>
+
+namespace throttle {
+namespace graphics {
+
+// returns vector of the required extensions.
+inline std::vector<const char *> get_required_extensions() {
+  uint32_t                  ext_count{};
+  auto                      arr_glfw_extensions = glfwGetRequiredInstanceExtensions(&ext_count);
+  std::vector<const char *> extensions{arr_glfw_extensions, arr_glfw_extensions + ext_count};
+  extensions.push_back("VK_EXT_debug_utils");
+  return extensions;
+}
+
+// returns vector of the requested validation layers.
+inline std::vector<const char *> get_required_layers() {
+#if defined(ENABLE_VALIDATION_LAYERS)
+  return std::vector<const char *>{"VK_LAYER_KHRONOS_validation"};
+#else
+  return {};
+#endif
+}
+
+// check if we can support all the required extensions in current context.
+inline bool extensions_supported(const std::vector<const char *> &extensions, const vk::raii::Context &context) {
+  auto supported_extensions = context.enumerateInstanceExtensionProperties();
+  bool found{};
+  for (auto extension : extensions) {
+    found = false;
+    for (auto &s_extension : supported_extensions)
+      if (std::strcmp(extension, s_extension.extensionName) == 0) found = true;
+    if (!found) return false;
+  }
+  return true;
+}
+
+// check if we can support all the layers.
+inline bool layers_supported(const std::vector<const char *> &layers) {
+  bool found{};
+  auto supported_layers = vk::enumerateInstanceLayerProperties();
+  for (auto layer : layers) {
+    found = false;
+    for (auto &s_layer : supported_layers)
+      if (std::strcmp(layer, s_layer.layerName) == 0) found = true;
+    if (!found) return false;
+  }
+  return true;
+}
+
+// check if we can support all the layer and extensions in current context.
+inline bool is_supported(const std::vector<const char *> &extensions, const std::vector<const char *> &layers,
+                         const vk::raii::Context &context) {
+  return extensions_supported(extensions, context) && layers_supported(layers);
+}
+
+// interface instance class.
+struct i_instance_data {
+  virtual vk::raii::Instance &instance() = 0;
+  virtual ~i_instance_data(){};
+};
+
+// concrete instance class with debug messenger.
+class instance_data : public i_instance_data {
+public:
+  instance_data() : m_handle{create_instance()}, m_dmessenger{create_debug_messenger(m_handle)} {}
+  vk::raii::Instance &instance() override { return m_handle; }
+
+private:
+  vk::raii::Instance create_instance() {
+    glfwInit();
+    vk::raii::Context   context;
+    auto                version = context.enumerateInstanceVersion();
+    vk::ApplicationInfo app_info{"Triangles intersection", version, "Best engine", version, version};
+    auto                extensions = get_required_extensions();
+    auto                layers = get_required_layers();
+    if (!is_supported(extensions, layers, context))
+      throw std::runtime_error("Not all the requested extensions are supported");
+    // clang-format off
+    vk::InstanceCreateInfo create_info {
+      vk::InstanceCreateFlags{},
+      &app_info, static_cast<uint32_t>(layers.size()),
+      layers.data(),
+      static_cast<uint32_t>(extensions.size()),
+      extensions.data()
+    };
+    // clang-format on
+    return vk::raii::Instance{context, create_info};
+  }
+
+  vk::raii::Instance               m_handle{nullptr};
+  vk::raii::DebugUtilsMessengerEXT m_dmessenger{nullptr};
+};
+} // namespace graphics
+} // namespace throttle
