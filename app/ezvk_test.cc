357bc2a61cd34b78c784dec371553ecbbabbe6f9
Sergei Zimmerman <serjtsimmerman@gmail.com>
2023-01-29 00:30:48 +0300

    Extend ezvk_test

--- app/ezvk_test.cc
+++ app/ezvk_test.cc
@@ -1,7 +1,9 @@
 #include "ezvk/debug.hpp"
 #include "ezvk/instance.hpp"
 
+#include "ezvk/window.hpp"
 #include "misc.hpp"
+#include <GLFW/glfw3.h>
 #include <exception>
 #include <iostream>
 #include <stdexcept>
@@ -10,7 +12,7 @@ int main() try {
   glfwInit();
 
   auto ext = triangles::required_vk_extensions();
-  auto layers = triangles::required_vk_layers();
+  auto layers = triangles::required_vk_layers(true);
 
   vk::raii::Context   ctx;
   vk::ApplicationInfo info = {.pApplicationName = "Hello, World!",
@@ -19,9 +21,14 @@ int main() try {
                               .pEngineName = "Junk Inc.",
                               .engineVersion = VK_MAKE_VERSION(1, 0, 0)};
 
-  ezvk::instance i = {ctx, info, ext.begin(), ext.end(), layers.begin(), layers.end()};
+  ezvk::instance           i = {ctx, info, ext.begin(), ext.end(), layers.begin(), layers.end()};
+  ezvk::debug_messenger    debug_msger{i()};
+  ezvk::unique_glfw_window w{"Hi, There", {800, 600}, true};
+  ezvk::surface            s{i(), w};
 
-  ezvk::debug_messenger debug_msger{i()};
+  while (!glfwWindowShouldClose(w())) {
+    glfwPollEvents();
+  }
 } catch (ezvk::unsupported_error &e) {
   std::cout << e.what() << "\n";
   for (const auto &v : e.missing()) {
