5e5be03a7ccc27c814b678508d143a19fbdbc451
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-01-29 11:51:07 +0300

    change vertices

--- app/application.hpp
+++ app/application.hpp
@@ -24,9 +24,9 @@
 
 namespace triangles {
 
-const std::vector<throttle::graphics::vertex> vertices{{{-5.0f, -5.0f, -1.0f}, {0.8f, 0.0f, 0.0f}},
-                                                       {{5.0f, -5.0f, 0.0f}, {0.0f, 1.0f, 0.0f}},
-                                                       {{0.5f, 0.5f, 5.0f}, {0.0f, 0.0f, 1.0f}},
+const std::vector<throttle::graphics::vertex> vertices{{{-0.5f, -0.5f, -1.0f}, {1.0f, 0.0f, 0.0f}},
+                                                       {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
+                                                       {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
                                                        {{-0.5f, 0.5f, 1.0f}, {1.0f, 1.0f, 1.0f}}};
 
 const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
