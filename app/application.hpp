0eca65568563199eaf5503216b520b3aed70ba97
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-01-30 14:44:32 +0300

    change vertices

--- app/application.hpp
+++ app/application.hpp
@@ -36,12 +36,21 @@
 
 namespace triangles {
 
-const std::vector<throttle::graphics::vertex> vertices{{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},
-                                                       {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
-                                                       {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
-                                                       {{-0.5f, 0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}}};
+// clang-format off
+const std::vector<throttle::graphics::vertex> vertices
+{
+// triangle 1
+{{-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+{{0.5f, -0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+{{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
+// triangle 2
+{{0.5f, 0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}},
+{{0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+{{0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}}
+};
+// clang-format on
 
-const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
+const std::vector<uint16_t> indices = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 constexpr int     MAX_FRAMES_IN_FLIGHT = 2;
 class application final {
